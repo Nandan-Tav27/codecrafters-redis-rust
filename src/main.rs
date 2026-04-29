b1d2b3bb5c45730237ad238d689a1cfa3df05092
Nandan-Tav27 <nandantavargeri@gmail.com>
2026-04-29 17:19:59 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     Ok(0) => return,
                     Ok(n) => {
                         let mut parser = resp::Parser::new(&in_buf[0..n]);
-                        if let Some(input) = parser.decode_array().unwrap()
-                            && input[0].to_lowercase() == "echo"
-                            && input.len() >= 2
-                        {
-                            let mut encoder = resp::Encoder::new(&mut out_buf);
-                            let len = encoder.encode_to_bulk_string(&input[1]).unwrap();
-                            let _ = socket.write(&out_buf[..len]).await;
+                        if let Some(input) = parser.decode_array().unwrap() {
+                            if input[0].to_lowercase() == "ping" {
+                                let _ = socket.write_all(b"+PONG\r\n").await;
+                            } else if input[0] == "echo" && input.len() >= 2 {
+                                let mut encoder = resp::Encoder::new(&mut out_buf);
+                                let len = encoder.encode_to_bulk_string(&input[1]).unwrap();
+                                let _ = socket.write(&out_buf[..len]).await;
+                            }
                         }
                     }
                     Err(e) => {
